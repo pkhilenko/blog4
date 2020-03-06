6162c517715c45766ad60ecb06e8554ebbde8df6
pkhilenko <pkhilenko@yandex.ru>
2020-03-06 12:23:10 +0600

    package_controller

--- src/main/java/web/controller/AdminUserController.java
+++ src/main/java/web/controller/AdminUserController.java
@@ -1,4 +1,4 @@
-package web;
+package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
--- src/main/java/web/controller/AuthController.java
+++ src/main/java/web/controller/AuthController.java
@@ -1,4 +1,4 @@
-package web;
+package web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
