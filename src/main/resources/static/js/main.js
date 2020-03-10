53df0b2f1946875fb28d327729caec721e259272
pkhilenko <pkhilenko@yandex.ru>
2020-03-10 14:52:50 +0600

    добавил бутстрап без модального окна

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,1 +1,9 @@
-console.log("проверка подключения");
+$(function() {
+    $('.admin-user').click(function(e) {
+        window.location.href='/admin/user';
+    });
+
+    $('.admin-show-form').click(function(e) {
+        window.location.href='/admin/showForm';
+    });
+});
