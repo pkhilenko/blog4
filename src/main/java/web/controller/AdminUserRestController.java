adb3ac488ea105bb09217666b0cb4d8c648aa12f
pkhilenko <pkhilenko@yandex.ru>
2020-03-12 15:16:42 +0600

    src/main/java/web/controller/AdminUserRestController.java

--- src/main/java/web/controller/AdminUserRestController.java
+++ src/main/java/web/controller/AdminUserRestController.java
@@ -1,12 +1,15 @@
 package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import web.model.User;
 import web.service.UserService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/admin")
@@ -20,50 +23,36 @@ public class AdminUserRestController {
         this.userService = userService;
     }
 
+    @ResponseBody
     @GetMapping("/user")
-    public ModelAndView listUsers(ModelAndView modelAndView) {
-        modelAndView.setViewName("admin-list-users");
-        modelAndView.addObject("users", userService.getUsers());
-        return modelAndView;
+    public List<User> listUsers() {
+        return  userService.getUsers();
     }
 
-    @GetMapping("/showForm")
-    public ModelAndView showFormForAdd(ModelAndView modelAndView) {
-        User user = new User();
-        modelAndView.setViewName("admin-user-form");
-        modelAndView.addObject("user", user);
-        return modelAndView;
-    }
-
+    ResponseBody
     @PostMapping("/saveUser")
-    public String saveUser(@ModelAttribute("user") User user) {
+    public User saveUser(@ModelAttribute("user") User user) {
         userService.saveUser(user);
-        return "redirect:/admin/user";
+        return user;
     }
 
-    @GetMapping("/updateForm/{userId}")
-    public ModelAndView showFormForUpdate(@PathVariable("userId") long id, ModelAndView modelAndView) {
-        modelAndView.setViewName("admin-user-form");
-        modelAndView.addObject("user", userService.getUser(id));
-        return modelAndView;
-    }
-
+    @ResponseBody
     @GetMapping("/delete/{userId}")
     public String deleteUser(@PathVariable("userId") long id) {
         userService.deleteUser(id);
-        return "redirect:/admin/user";
+        return HttpStatus.OK.toString();
     }
 
+    @ResponseBody
     @GetMapping("/user/{name}")
-    public ModelAndView userEditForm(@PathVariable String name, ModelAndView modelAndView) {
+    public User userEditForm(@PathVariable String name) {
         User user = userService.findByUsername(name);
-        modelAndView.setViewName("admin-user-details");
-        modelAndView.addObject("user", user);
-        return modelAndView;
+        return user;
     }
+
+    @ResponseBody
 
     @GetMapping("/findOne")
-    @ResponseBody
     public User findOne(Long id) {
         return userService.getUser(id);
     }
--- src/main/java/web/dao/UserDaoImpl.java
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -1,6 +1,5 @@
 package web.dao;
 
-import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import web.model.User;
